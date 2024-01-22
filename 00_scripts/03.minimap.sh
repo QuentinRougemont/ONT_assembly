10800fcba20c80647a33326038e9020ae398572a
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-22 17:54:48 +0100

    remove slurm specific code

--- 00_scripts/03.minimap.sh
+++ 00_scripts/03.minimap.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=mini                
-#SBATCH --output=log_mini-%J.out                  
-#SBATCH --cpus-per-task=8                   
-#SBATCH --mem=20G                           
-                                             
-# Move to directory where job was submitted  
-cd $SLURM_SUBMIT_DIR                         
 
-source /local/env/envgcc-9.3.0.sh
-source /local/env/envconda3.sh 
-conda activate /home/genouest/cnrs_umr5175/qrougemont/mes_envs/samtools1.15/
-. /local/env/envminimap2-2.15.sh
-input=$1
+input=$1 #genome basename 
 ref=assembly.fasta
 
 cd 03.genome
