d9bc8f9d89f86a7b722161ae8dbfdde0a160421d
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-24 16:06:57 +0100

    Update 03.minimap.sh

--- 00_scripts/03.minimap.sh
+++ 00_scripts/03.minimap.sh
@@ -6,3 +6,15 @@ ref=assembly.fasta
 cd 03.genome
 minimap2 -ax map-ont $input/"$input"_flye/$ref  $input/*.fastq.gz |\
 	 samtools view -Sb - |samtools sort - > $input/"$input".aligned.sorted.bam
+
+samtools depth $input/"$intpu".aligned.sorted.bam |gzip > "$input"/depth.txt.gz
+
+#compute mean depth: 
+zcat "$input"/depth.txt.gz |awk -F'\t' ' { sum[$1] += $3; N[$1]++ } 
+          END     { for (key in sum) {
+                        avg = sum[key] / N[key];
+                        printf "%s %f\n", key, avg;
+                    } }'  > "$input"/mean_dp.txt
+
+cd $input
+Rscript ../00_scripts/plot_depth.R 
