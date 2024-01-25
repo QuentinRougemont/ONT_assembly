807f243e7d3546ee53468dd571b14ec5d76339cd
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-25 11:08:38 +0100

    Update 02.flye.sh

--- 00_scripts/02.flye.sh
+++ 00_scripts/02.flye.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
-##SBATCH --job-name=flye                
-##SBATCH --output=log_flye-%J.out                  
-##SBATCH --cpus-per-task=8                   
-##SBATCH --mem=90G                           
-                                             
-# Move to directory where job was submitted  
-conda activate flye
 
 genome=$1 #name of the genome to assemble
 
 # Copy script as it was run                 
-TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
-SCRIPT=$0
-NAME=$(basename $0)
-LOG_FOLDER="LOG_FLYE/"
-mkdir $LOG_FOLDER 2>/dev/null
-cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
-
 echo running flye on $genome 
 
 cd 02.FilteredRaw/$genome
 
-pwd
-
 flye --nano-raw *fastq.gz --genome-size 30m -o "$genome"_flye -t8
 
-
+#other option that may be relevant: --asm 50 
+# --meta (in case there's putative contaminants)
